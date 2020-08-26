43902e766950a92fd0273f070c7a0986742cf1d3
David Hales <haldw@live.co.uk>
2020-08-26 15:47:55 +0400

    Update index.js
    Database expanded.

--- index.js
+++ index.js
@@ -7,151 +7,599 @@ let bondMovies = [
   {
     title: 'Dr. No',
     year: '1962',
-    bond: 'Sean Connery',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Terence Young',
+      dob: '20 June 1915',
+      pob: 'Shanghai, China',
+      dod: '07 September 1994'
+    },
+    bond: {
+      name:'Sean Connery',
+      dob: '25 August 1930',
+      pob: 'Edinburgh, Scotland',
+      dod: '--'
+    },
+    plot: 'Bond searches for a missing collleague amid deliberate disruption of the American space program.',
+    studio: 'United Artists',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_Dr_No.jpg'
   },
   {
     title: 'From Russia With Love',
     year: '1963',
-    bond: 'Sean Connery',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Terence Young',
+      dob: '20 June 1915',
+      pob: 'Shanghai, China',
+      dod: '07 September 1944'
+    },
+    bond: {
+      name:'Sean Connery',
+      dob: '25 August 1930',
+      pob: 'Edinburgh, Scotland',
+      dod: '--'
+    },
+    plotline: 'Bond goes in search of a Soviet decoding machine, needing to find it before the evil S.P.E.C.T.R.E organisation',
+    studio: 'United Artists',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_From_Russia_With_Love.jpg'
   },
   {
     title: 'Goldfinger',
     year: '1964',
-    bond: 'Sean Connery',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Guy Hamilton',
+      dob: '16 September 1922',
+      pob: 'Paris, France',
+      dod: '20 April 2016'
+    },
+    bond: {
+      name:'Sean Connery',
+      dob: '25 August 1930',
+      pob: 'Edinburgh, Scotland',
+      dod: '--'
+    },
+    plotline: 'Auric Goldfinger has everything but the Fort Knox US Gold Reserve.  Now he wants that too.',
+    studio: 'United Artists',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_Goldfinger.jpg'
   },
   {
     title: 'Thunderball',
     year: '1965',
-    bond: 'Sean Connery',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Terence Young',
+      dob: '20 June 1915',
+      pob: 'Shanghai, China',
+      dod: '07 September 1944'
+    },
+    bond: {
+      name:'Sean Connery',
+      dob: '25 August 1930',
+      pob: 'Edinburgh, Scotland',
+      dod: '--'
+    },
+    plotline: 'Evidence leads Bond to the Bahamas in search of missing nuclear weapons.',
+    studio: 'United Artists',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_Thunderball.jpg'
   },
   {
     title: 'You Only Live Twice',
     year: '1967',
-    bond: 'Sean Connery',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Lewis Gilbert',
+      dob: '06 March 1920',
+      pob: 'London, England',
+      dod: '23 February 2018'
+    },
+    bond: {
+      name:'Sean Connery',
+      dob: '25 August 1930',
+      pob: 'Edinburgh, Scotland',
+      dod: '--'
+    },
+    plotline: 'Bond discovers the evil S.P.E.C.T.R.E is back, this time provoking war in space between the United States and Soviet Union',
+    studio: 'United Artists',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_You_Only_Live_Twice.jpg'
   },
   {
     title: 'Casino Royale (1967)',
     year: '1967',
-    bond: 'Peter Sellers',
+    genres: [
+      'Comedy',
+    ],
+    director: {
+      name: 'Val Guest',
+      dob: '11 December 1911',
+      pob: 'Maida Vale, England',
+      dod: '10 May 2006'
+    },
+    bond: {
+      name:'Peter Sellers',
+      dob: '08 September 1925',
+      pob: 'Southsea, England',
+      dod: '24 July 1980'
+    },
+    plotline: 'Bond spoof.  Evelyn Tremble (a.k.a James Bond 007) comes from retirement to tackle the evil S.M.E.R.S.H organisation',
+    studio: 'Columbia Pictures',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_Casino_Royale_1967.jpg'
   },
   {
     title: 'On Her Majesty\'s Secret Service',
     year: '1969',
-    bond: 'George Laxenby',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Peter Hunt',
+      dob: '11 March 1925',
+      pob: 'London, England',
+      dod: '14 August 2002'
+    },
+    bond: {
+      name:'George Lazenby',
+      dob: '05 September 1939',
+      pob: 'Queanbeyan, Australia',
+      dod: '--'
+    },
+    plotline: 'Bond investigates the true motives behind an allergy research centre in the Swiss Alps.',
+    studio: 'United Artists',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_OHMSS.jpg'
   },
   {
     title: 'Diamonds Are Forever',
     year: '1971',
-    bond: 'Sean Connery',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Guy Hamilton',
+      dob: '16 September 1922',
+      pob: 'Paris, France',
+      dod: '20 April 2016'
+    },
+    bond: {
+      name:'Sean Connery',
+      dob: '25 August 1930',
+      pob: 'Edinburgh, Scotland',
+      dod: '--'
+    },
+    plotline: 'A diamond smuggling operation takes Bond to the lights and darkness of Las Vegas',
+    studio: 'United Artists',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_Diamonds_Are_Forever.jpg'
   },
   {
     title: 'Live and let Die',
     year: '1973',
-    bond: 'Roger Moore',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Guy Hamilton',
+      dob: '16 September 1922',
+      pob: 'Paris, France',
+      dod: '20 April 2016'
+    },
+    bond: {
+      name:'Roger Moore',
+      dob: '14 October 1927',
+      pob: 'Stockwell, England',
+      dod: '23 May 2017'
+    },
+    plotline: 'Heroin smuggling in the US southern bayou has Bond facing tarot cards and witch doctory',
+    studio: 'United Artists',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_Live_And_Let_Die.jpg'
   },
   {
     title: 'The Man With the Golden Gun',
     year: '1974',
-    bond: 'Roger Moore',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Guy Hamilton',
+      dob: '16 September 1922',
+      pob: 'Paris, France',
+      dod: '20 April 2016'
+    },
+    bond: {
+      name:'Roger Moore',
+      dob: '14 October 1927',
+      pob: 'Stockwell, England',
+      dod: '23 May 2017'
+    },
+    plotline: 'Who is killing scientists?  And who is Francisco Scaramanga?',
+    studio: 'United Artists',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_The_Man_With_The_Golden_Gun.jpg'
   },
   {
     title: 'The Spy Who Loved me',
     year: '1977',
-    bond: 'Roger Moore',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Lewis Gilbert',
+      dob: '06 March 1920',
+      pob: 'London, England',
+      dod: '23 February 2018'
+    },
+    bond: {
+      name:'Roger Moore',
+      dob: '14 October 1927',
+      pob: 'Stockwell, England',
+      dod: '23 May 2017'
+    },
+    plotline: 'Someone is hijacking nuclear submarines. Bond needs to stop them before the world goes to war.',
+    studio: 'United Artists',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_The_Spy_Who_Loved_Me.jpg'
   },
   {
     title: 'Moonraker',
     year: '1979',
-    bond: 'Roger Moore',
+    genres: [
+      'Action',
+      'Adventure',
+      'Sci-Fi'
+    ],
+    director: {
+      name: 'Lewis Gilbert',
+      dob: '06 March 1920',
+      pob: 'London, England',
+      dod: '23 February 2018'
+    },
+    bond: {
+      name:'Roger Moore',
+      dob: '14 October 1927',
+      pob: 'Stockwell, England',
+      dod: '23 May 2017'
+    },
+    plotline: 'Someone has stolen one of NASA\'s spaceshuttles. Bond needs to find out who.',
+    studio: 'United Artists',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_Moonraker.jpg'
   },
   {
     title: 'For Your Eyes Only',
     year: '1981',
-    bond: 'Roger Moore',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'John Glen',
+      dob: '15 May 1932',
+      pob: 'Sunbury-on-Thames, England',
+      dod: '--'
+    },
+    bond: {
+      name:'Roger Moore',
+      dob: '14 October 1927',
+      pob: 'Stockwell, England',
+      dod: '23 May 2017'
+    },
+    plotline: 'A missing Royal Navy intelligence vessel was equipped with a top secret weapons encryption device.  Bond needs to find it fast.',
+    studio: 'United Artists',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_For_Your_Eyes_Only.jpg'
   },
   {
     title: 'Octopussy',
     year: '1983',
-    bond: 'Roger Moore',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'John Glen',
+      dob: '15 May 1932',
+      pob: 'Sunbury-on-Thames, England',
+      dod: '--'
+    },
+    bond: {
+      name:'Roger Moore',
+      dob: '14 October 1927',
+      pob: 'Stockwell, England',
+      dod: '23 May 2017'
+    },
+    plotline: 'A fake Fabergé egg leads to a jewel smuggling operation and the mysterious Octopussy.',
+    studio: 'United Artists',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_Octopussy.jpg'
   },
   {
     title: 'Never Say Never Again',
     year: '1983',
-    bond: 'Sean Connery',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Irvin Kershner',
+      dob: '23 April 1929',
+      pob: 'Philadelphia, USA',
+      dod: '27 November 2010'
+    },
+    bond: {
+      name:'Sean Connery',
+      dob: '25 August 1930',
+      pob: 'Edinburgh, Scotland',
+      dod: '--'
+    },
+    plotline: 'Thunderball remake.  Someone has stolen two US nuclear weapons.',
+    studio: 'Warner Bros',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_Never_Say_Never_Again.jpg'
   },
   {
     title: 'A View To A Kill',
     year: '1985',
-    bond: 'Roger Moore',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'John Glen',
+      dob: '15 May 1932',
+      pob: 'Sunbury-on-Thames, England',
+      dod: '--'
+    },
+    bond: {
+      name:'Roger Moore',
+      dob: '14 October 1927',
+      pob: 'Stockwell, England',
+      dod: '23 May 2017'
+    },
+    plotline: 'Racehorses and microchips. A genius industrialist. Bond needs to find out what they all have in common.',
+    studio: 'MGM/UA',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/img/007_A_View_To_A_Kill.jpg'
   },
   {
     title: 'The Living Daylights',
     year: '1987',
-    bond: 'Timothy Dalton',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'John Glen',
+      dob: '15 May 1932',
+      pob: 'Sunbury-on-Thames, England',
+      dod: '--'
+    },
+    bond: {
+      name:'Timothy Dalton',
+      dob: '21 March 1946',
+      pob: 'Colwyn Bay, Wales',
+      dod: '--'
+    },
+    plotline: 'Bond investigates a suspected KGB policy to hunt and kill enemy agents.',
+    studio: 'United Artists',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_The Living Daylights.jpg'
   },
   {
     title: 'Licence to Kill',
     year: '1989',
-    bond: 'Timothy Dalton',
+    genres: [
+      'Action',
+      'Adventure',
+      'Crime'
+    ],
+    director: {
+      name: 'John Glen',
+      dob: '15 May 1932',
+      pob: 'Sunbury-on-Thames, England',
+      dod: '--'
+    },
+    bond: {
+      name:'Timothy Dalton',
+      dob: '21 March 1946',
+      pob: 'Colwyn Bay, Wales',
+      dod: '--'
+    },
+    plotline: 'Bond\'s friend, Felix Leiter, is left for dead on his wedding day. Bond\'s investigation leads to straight to a drug lord.',
+    studio: 'United Artists',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_Licence To Kill.jpg'
   },
   {
     title: 'GoldenEye',
     year: '1995',
-    bond: 'Pierce Brosnan',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Martin Campbell',
+      dob: '24 October 1943',
+      pob: 'Hastings, New Zealand',
+      dod: '--'
+    },
+    bond: {
+      name:'Pierce Brosnan',
+      dob: '16 May 1953',
+      pob: 'Drogheda, Ireland',
+      dod: '--'
+    },
+    plotline: 'The theft of secret satellite technology leads Bond to a Russian crime syndicate, and a once friend he had watched die years earlier.',
+    studio: 'United Artists',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_Goldeneye.jpg'
   },
   {
     title: 'Tomorrow Never Dies',
     year: '1997',
-    bond: 'Pierce Brosnan',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Roger Spottiswoode',
+      dob: '05 January 1945',
+      pob: 'Ottawa, Canada',
+      dod: '--'
+    },
+    bond: {
+      name:'Pierce Brosnan',
+      dob: '16 May 1953',
+      pob: 'Drogheda, Ireland',
+      dod: '--'
+    },
+    plotline: 'A media maganate foments war between China and the UK in the belief that ratings are everything.',
+    studio: 'MGM/UA',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_Tomorrow_Never_Dies.jpg'
   },
   {
     title: 'The World Is Not Enough',
     year: '1999',
-    bond: 'Pierce Brosnan',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Michael Apted',
+      dob: '10 February 1941',
+      pob: 'Aylesbury, England',
+      dod: '--'
+    },
+    bond: {
+      name:'Pierce Brosnan',
+      dob: '16 May 1953',
+      pob: 'Drogheda, Ireland',
+      dod: '--'
+    },
+    plotline: 'Bond uncovers a nuclear plot whilst rescuing a former heiress from her kidnapper.',
+    studio: 'MGM',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_The_World_Is_Not_Enough.jpg'
   },
   {
     title: 'Die Another Day',
     year: '2002',
-    bond: 'Pierce Brosnan',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Lee Tamahori',
+      dob: '22 April 1950',
+      pob: 'Wellington, New Zealand',
+      dod: '--'
+    },
+    bond: {
+      name:'Pierce Brosnan',
+      dob: '16 May 1953',
+      pob: 'Drogheda, Ireland',
+      dod: '--'
+    },
+    plotline: 'Diamonds and a North Korean terrorist. Bond searches for the link to a new space weapon',
+    studio: 'MGM',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_Die_Another_Day.jpg'
   },
   {
     title: 'Casino Royale (2006)',
     year: '2006',
-    bond: 'Daniel Craig',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Martin Campbell',
+      dob: '24 October 1943',
+      pob: 'Hastings, New Zealand',
+      dod: '--'
+    },
+    bond: {
+      name:'Daniel Craig',
+      dob: '02 March 1968',
+      pob: 'Chester, England',
+      dod: '--'
+    },
+    plotline: 'Based on the original Fleming Casino Royale, Bond must defeat a private banker in a agme of high stakes poker.',
+    studio: 'Columbia Pictures',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_Casino_Royale.jpg'
   },
   {
     title: 'Skyfall',
     year: '2012',
-    bond: 'Daniel Craig',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Sam Mendes',
+      dob: '01 August 1965',
+      pob: 'Reading, England',
+      dod: '--'
+    },
+    bond: {
+      name:'Daniel Craig',
+      dob: '02 March 1968',
+      pob: 'Chester, England',
+      dod: '--'
+    },
+    plotline: 'Skeleton\'s in the closet. A secret from M\'s past.  What have they in common when M16 comes under direct attack?',
+    studio: 'MGM/Columbia Pictures',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_Skyfall.jpg'
   },
   {
     title: 'Spectre',
     year: '2015',
-    bond: 'Daniel Craig',
+    genres: [
+      'Action',
+      'Adventure',
+      'Thriller'
+    ],
+    director: {
+      name: 'Sam Mendes',
+      dob: '01 August 1965',
+      pob: 'Reading, England',
+      dod: '--'
+    },
+    bond: {
+      name:'Daniel Craig',
+      dob: '02 March 1968',
+      pob: 'Chester, England',
+      dod: '--'
+    },
+    plotline: 'Bond is on the trail of a secret organisation called S.P.E.C.T.R.E.  How does it link to the awful events of the recent past?',
+    studio: 'MGM/Columbia Pictures',
     posterURL: 'http://www.sweepback.co.uk/bondMovies/img/007_Spectre.jpg'
   },
 ];
