68088a7468e9616920b4d6d612f2712fb9a805e8
David <haldw@live.co.uk>
2020-09-15 14:08:53 +0400

    Update index.js
    Environment variable added to replace connection URI

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const Genres = Models.Genre;
 const Directors = Models.Director;
 const Actors = Models.Actor;
 
-mongoose.connect('mongodb://localhost:27017/bondMovieDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/bondMovieDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
 
 const express = require('express'),
