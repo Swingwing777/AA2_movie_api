2ec05dc4b67047a4c1978bb822da8aad9ae74789
David <haldw@live.co.uk>
2020-09-24 08:06:11 +0400

    Parcel rebuild

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -30328,7 +30328,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43765" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46499" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
