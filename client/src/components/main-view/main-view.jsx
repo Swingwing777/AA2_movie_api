8d8d7596b0f82f941348e75ed2e9f23971220f6a
David <haldw@live.co.uk>
2020-09-29 11:46:54 +0400

    Main View styled
    Still having problems with memory leak when closing Movie View

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import axios from 'axios';
 
 import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import CardDeck from 'react-bootstrap/CardDeck';
+import './main-view.scss';
 
 export class MainView extends React.Component {
 
@@ -47,17 +50,21 @@ export class MainView extends React.Component {
 
     if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
+    //if (!user) return <RegistrationView />;
+
     // Before the movies have been loaded
     if (!movies) return <div className="main-view" />;
 
     return (
       <div className="main-view">
-        {selectedMovie
-          ? <MovieView movie={selectedMovie} />
-          : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)} />
-          ))
-        }
+        <CardDeck className='d-flex flex-wrap'>
+          {selectedMovie
+            ? <MovieView movie={selectedMovie} />
+            : movies.map(movie => (
+              <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)} />
+            ))
+          }
+        </CardDeck>
       </div>
     );
   }
