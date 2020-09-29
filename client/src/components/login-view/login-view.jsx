9d3ea79eaecd1a5771a7478fcad13eb5ab69b2b1
David <haldw@live.co.uk>
2020-09-29 11:05:07 +0400

    Login-View basic build and style

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -1,27 +1,69 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import './login-view.scss';
+import { RegistrationView } from '../registration-view/registration-view';
 
 export function LoginView(props) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [Username, setUsername] = useState('');
+  const [Password, setPassword] = useState('');
+
+  const registerUser = () => {
+    console.log('New User Required');
+    return <RegistrationView />;
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
+    console.log(Username, Password);
     // Send a request to the server for authentication then call props.onLoggedIn(username)
-    props.onLoggedIn(username);
+    props.onLoggedIn(Username);
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button type="button" onClick={handleSubmit}>Submit</button>
-    </form>
+    <Container className='formwrapper' fluid='md'>
+      <Form className='p-md-3'>
+        <Form.Row className='d-flex flex-md-row justify-content-center'>
+          <Form.Group className='p-md-3' controlId='formUsername'>
+            <Form.Label className='p-md-3 formLabel'>Username</Form.Label>
+            <Form.Control
+              type='text'
+              value={Username}
+              onChange={e => setUsername(e.target.value)}
+              placeholder='Enter Username'
+            />
+          </Form.Group>
+
+          <Form.Group className='p-md-3' controlId='formPassword'>
+            <Form.Label className='p-md-3 formLabel'>Password</Form.Label>
+            <Form.Control
+              type='password'
+              value={Password}
+              onChange={e => setPassword(e.target.value)}
+              placeholder='Enter Password' />
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Row className='py-3 d-flex justify-content-center'>
+          <Button as={Col} xs={1} className='formButton' variant='primary' type='submit' onClick={handleSubmit} >
+            Submit
+          </Button>
+        </Form.Row>
+        <Form.Row className='py-3 d-flex justify-content-center'>
+          <Button as={Col} xs={1} className='formButton' variant='primary' type='submit' onClick={registerUser} >
+            Register New User
+          </Button>
+        </Form.Row>
+      </Form>
+    </Container>
   );
 }
+
+LoginView.propTypes = {
+  Username: PropTypes.string,
+  Password: PropTypes.string
+};
