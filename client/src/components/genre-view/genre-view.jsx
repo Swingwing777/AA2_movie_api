3c4bd07e29a3fa2397b81c4653135d23d7992e5b
David <haldw@live.co.uk>
2020-10-02 16:51:31 +0400

    Skeleton components created for Profile, Genres and Directors View

---
+++ client/src/components/genre-view/genre-view.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import { Form, Container, Button, Col } from 'react-bootstrap';
+import './genre-view.scss';
+import { RegistrationView } from '../registration-view/registration-view';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+GenreView.propTypes = {
+
+};
---
+++ client/src/components/profile-view/profile-view.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import { Form, Container, Button, Col } from 'react-bootstrap';
+import './profile-view.scss';
+import { RegistrationView } from '../registration-view/registration-view';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ProfileView.propTypes = {
+
+};
