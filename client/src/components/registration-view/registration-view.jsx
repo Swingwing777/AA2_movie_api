b1c39f186774f35a2f437c8fef6d00a822716047
David <haldw@live.co.uk>
2020-10-05 15:45:46 +0400

    Updated registration view - confirmed functional

--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import { LoginView } from '../login-view/login-view';
-import { Form, Container, Col, Button } from 'react-bootstrap';
+import { Form, Container, Col, Button, Row } from 'react-bootstrap';
 import './registration-view.scss';
 
-
+import { Link } from "react-router-dom";
 
 export function RegistrationView(props) {
   const [username, setUsername] = useState('');
@@ -31,25 +31,26 @@ export function RegistrationView(props) {
       });
   };
 
-  const loginUser = (e) => {
-    e.preventDefault();
-    setUsername('Registered');
-    props.onLoggedIn(username);
-  };
+  // const loginUser = (e) => {
+  //   e.preventDefault();
+  //   setUsername('Registered');
+  //   props.onLoggedIn(username);
+  // };
 
   if (username === 'Registered') return <LoginView onLoggedIn={user => this.handleSubmit(user)} />;
 
   return (
-    <Container className='formwrapper' fluid='md'>
+    <Container className='formwrapper' >
       <Form className='p-md-3'>
         <Form.Row className='d-flex flex-md-row justify-content-center'>
-          <Form.Label size='lg' className='formLabel'>Please Register</Form.Label>
+          <Form.Label size='lg' className='formTitle'>Please Register</Form.Label>
         </Form.Row>
 
-        <Form.Row className='d-flex flex-md-row justify-content-center'>
-          <Form.Group as={Col} xs={3} controlId='formGridUsername'>
+        <Form.Row className='justify-content-center mt-3'>
+          <Form.Group as={Col} controlId='formGridUsername'>
             <Form.Label className='formLabel'>Username</Form.Label>
             <Form.Control
+              className='entryField'
               type='text'
               value={username}
               onChange={e => setUsername(e.target.value)}
@@ -57,9 +58,10 @@ export function RegistrationView(props) {
             />
           </Form.Group>
 
-          <Form.Group as={Col} xs={3} controlId='formGridPassword'>
+          <Form.Group as={Col} controlId='formGridPassword'>
             <Form.Label className='formLabel'>Password</Form.Label>
             <Form.Control
+              className='entryField'
               type='password'
               value={password}
               onChange={e => setPassword(e.target.value)}
@@ -68,36 +70,39 @@ export function RegistrationView(props) {
         </Form.Row>
 
         <Form.Row className='d-flex flex-md-row justify-content-center'>
-          <Form.Group as={Col} xs={3} controlId='formGridEmail'>
+          <Form.Group as={Col} controlId='formGridEmail'>
             <Form.Label className='formLabel'>Email</Form.Label>
             <Form.Control
+              className='entryField'
               type='email'
               value={email}
               onChange={e => setEmail(e.target.value)}
               placeholder='Enter Email' />
-            <Form.Text className='formPromise'>We will never share your details</Form.Text>
+
           </Form.Group>
 
-          <Form.Group as={Col} xs={3} controlId='formBirthday'>
+          <Form.Group as={Col} controlId='formBirthday'>
             <Form.Label className='formLabel'>Birthday</Form.Label>
             <Form.Control
+              className='entryField'
               type='date'
               value={birthday}
               onChange={e => setBirthday(e.target.value)}
               placeholder='Enter Birthday' />
           </Form.Group>
         </Form.Row>
+        <Row className='formPromise'>We will never share your details</Row>
 
-        <Form.Row className='d-flex flex-md-row justify-content-center'>
-          <Button as={Col} xs={1} className='px-2 mr-2 formButton' variant='primary' type='submit' onClick={registerUser} >
+        <Form.Row className='justify-content-center'>
+          <Button className='formButton mt-3' variant='primary' type='submit' onClick={registerUser} >
             Submit
         </Button>
-
         </Form.Row>
-        <Form.Row className='py-3 d-flex justify-content-center'>
-          <Button as={Col} xs={1} className='formButton' variant='primary' type='submit' onClick={loginUser} >
-            Return to Login
-          </Button>
+
+        <Form.Row className='justify-content-center'>
+          <Link to={`/`}>
+            <Button className='formButton mt-3' variant="link">Return to Login</Button>
+          </Link>
         </Form.Row>
       </Form >
     </Container>
