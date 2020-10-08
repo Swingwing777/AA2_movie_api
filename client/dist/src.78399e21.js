ebd3868c8ad92dabaa7bae2fc830c78243f199ab
David <haldw@live.co.uk>
2020-10-08 05:57:36 +0400

    rebuild

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -56725,7 +56725,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "32895" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35659" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
