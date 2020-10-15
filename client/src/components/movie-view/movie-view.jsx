59f1a6c4f687cfd2d9498f6b29ab28d04108f891
David <haldw@live.co.uk>
2020-10-15 17:37:25 +0400

    Update movie-view.jsx
    Styling

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import { Container, Button, Row, Card, Col } from 'react-bootstrap';
 
 import { Link } from "react-router-dom";
 
+import { setMovies } from '../../actions/actions';
+import { cancelToken } from '../../actions/actions';
+import { setUserProf } from '../../actions/actions';
 import './movie-view.scss';
 
 export class MovieView extends React.Component {
@@ -13,63 +17,112 @@ export class MovieView extends React.Component {
     this.state = {}
   }
 
-  render() {
+  getUser(token) {
+    let user = localStorage.getItem('user')
+    const source = axios.CancelToken.source();
+    axios.get(`https://cors-anywhere.herokuapp.com/bond-movie-api.herokuapp.com/users/${user}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(response => {
+        setUserProf(response.data);                           // Change 2 (not 'setState')
+      })
+      .catch(e => {
+        console.log('User error: ' + e);
+      })
+    axios.get("https://jsonplaceholder.typicode.com/todos", {
+      cancelToken: source.token
+    }).then(response => {
+      cancelToken(response.data);                             // Change 3 (not 'setState')
+    }).catch(e => {
+      console.log('Cancel Token error: ' + e)
+    });
+  }
+
+  componentDidMount() {
+    let accessToken = localStorage.getItem('token');
+    this.getUser(accessToken);
+  }
+
+  addFavorite(movieId) {
     const { movie } = this.props;
-    console.log(this.props)
+    let user = localStorage.getItem('user');
+    axios.post(`https://cors-anywhere.herokuapp.com/bond-movie-api.herokuapp.com/users/${user}/movieID/${movieId} `, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+    })
+      .then(response => {
+        alert('Movie successfully added to favorites');
+      })
+      .catch(e => {
+        alert(`${movie.Title} could not be added to your favorites ` + e)
+      });
+  }
+
+  deleteFavorite(movieId) {
+    let user = localStorage.getItem('user');
+    axios.delete(`https://cors-anywhere.herokuapp.com/bond-movie-api.herokuapp.com/users/${user}/movieID/${movieId} `, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+    })
+      .then(response => {
+        setUserProf(response.data);
+        alert('Movie successfully deleted from favorites');
+      })
+      .catch(e => {
+        alert('Movie could not be deleted from favorites ' + e)
+      });
+  }
+
+  render() {
+    const { movie, userProfile } = this.props;
+    // console.log(this.props)
     if (!movie) return null;
 
     return (
-      <Container className='d-flex justify-content-center' >
-        <Card style={{ width: 'fit-content(80%)' }} className='m-3 h-160 text-center movie-card'>
+      <Container style={{ width: '100%' }} className='d-flex justify-content-center' >
+        <Card style={{ width: '100%' }} className='px-0 m-1 h-160 text-center movie-card'>
           <Card.Body className='cardBody p-1'>
-            <Row className='p-2 justify-content-center'>
-              <Col>
-                <Card.Img variant="left" className='movie-poster' src={movie.ImagePath} />
-              </Col>
-              <Col>
-                <Card.Title className='titleh1 mt-3'>{movie.Title}</Card.Title>
-                <Card.Text className='text-left mt-3'>
-                  <span className='label'>{'Director:\u00A0\u00A0'}</span>
-                  <span className='value'>{movie.Director.Name}</span>
-                </Card.Text>
-                <Card.Text className='text-left mt-3'>
-                  <span className='label'>{'Release Year:\u00A0\u00A0'} </span>
-                  <span className='value'>{movie.Year}</span>
-                </Card.Text>
-                <Card.Text className='text-left mt-3'>
-                  <span className='label'>{'Plot Summary:\u00A0\u00A0'}{movie.Description}</span>
-                </Card.Text>
-                <Card.Text className='text-left mt-3'>
-                  <span className='label'>{'Genre:\u00A0\u00A0'}</span>
-                  <span className='value'>{movie.Genre.Name}</span>
-                </Card.Text>
-                <Card.Text className='text-left mt-3'>
-                  <span className='label'>{'Bond:\u00A0\u00A0'}</span>
-                  <span className='value'>{movie.BondActor.Name}</span>
-                </Card.Text>
-                <Card.Text className='text-left mt-3'>
-                  <span className='label'>{'Heroine:\u00A0\u00A0'}</span>
-                  <span className='value'>{movie.Heroine}</span>
-                </Card.Text>
-                <Card.Text className='text-left mt-3'>
-                  <span className='label'>{'Villain:\u00A0\u00A0'}</span>
-                  <span className='value'>{movie.Villain}</span>
-                </Card.Text>
-                <Card.Text className='text-left mt-3'>
-                  <span className='label'>{'Supporting Cast:\u00A0\u00A0'}</span>
-                  <span className='value'>{movie.Actors.join(', ')}</span>
-                </Card.Text>
-                <Card.Text className='text-left mt-3'>
-                  <span className='label'>{'Title Song by:\u00A0\u00A0'}</span>
-                  <span className='value'>{movie.SongArtist}</span>
-                </Card.Text>
-              </Col>
-            </Row>
+            <Card.Img style={{ width: '30%' }} variant="top" className='movie-poster' src={movie.ImagePath} />
+            <Card.Title className='titleh1 mt-3'>{movie.Title}</Card.Title>
+            <Card.Text className='text-left mt-3'>
+              <span className='label'>{'Director:\u00A0\u00A0'}</span>
+              <span className='value'>{movie.Director.Name}</span>
+            </Card.Text>
+            <Card.Text className='text-left mt-3'>
+              <span className='label'>{'Release Year:\u00A0\u00A0'} </span>
+              <span className='value'>{movie.Year}</span>
+            </Card.Text>
+            <Card.Text className='text-left mt-3'>
+              <span className='label'>{'Plot Summary:\u00A0\u00A0'}{movie.Description}</span>
+            </Card.Text>
+            <Card.Text className='text-left mt-3'>
+              <span className='label'>{'Genre:\u00A0\u00A0'}</span>
+              <span className='value'>{movie.Genre.Name}</span>
+            </Card.Text>
+            <Card.Text className='text-left mt-3'>
+              <span className='label'>{'Bond:\u00A0\u00A0'}</span>
+              <span className='value'>{movie.BondActor.Name}</span>
+            </Card.Text>
+            <Card.Text className='text-left mt-3'>
+              <span className='label'>{'Heroine:\u00A0\u00A0'}</span>
+              <span className='value'>{movie.Heroine}</span>
+            </Card.Text>
+            <Card.Text className='text-left mt-3'>
+              <span className='label'>{'Villain:\u00A0\u00A0'}</span>
+              <span className='value'>{movie.Villain}</span>
+            </Card.Text>
+            <Card.Text className='text-left mt-3'>
+              <span className='label'>{'Supporting Cast:\u00A0\u00A0'}</span>
+              <span className='value'>{movie.Actors.join(', ')}</span>
+            </Card.Text>
+            <Card.Text className='text-left mt-3'>
+              <span className='label'>{'Title Song by:\u00A0\u00A0'}</span>
+              <span className='value'>{movie.SongArtist}</span>
+            </Card.Text>
+
 
           </Card.Body>
 
           <Card.Footer className="cardFoot border-top-0">
-            <Row className='mt-3 d-flex flex-md-row justify-content-center'>
+            <Row className='mt-1 d-flex flex-md-row justify-content-center'>
               <Link to={`/`}>
                 <Button className='goFacts m-3' variant="link">Main Menu</Button>
               </Link>
@@ -77,13 +130,26 @@ export class MovieView extends React.Component {
                 <Button className='goFacts m-3' variant="link">James Bond</Button>
               </Link>
             </Row>
-            <Row className='mt-3 d-flex flex-md-row justify-content-center'>
+            <Row className='mt-1 d-flex flex-md-row justify-content-center'>
               <Link to={`/directors/${movie.Title}/${movie.Director.Name}`}>
                 <Button className='goFacts m-3' variant="link">Director</Button>
               </Link>
               <Link to={`/genres/${movie.Title}/${movie.Genre.Name}`}>
                 <Button className='goFacts m-3' variant="link">Genre</Button>
               </Link>
+              <Button className="goAdd m-3" onClick={() => this.addFavorite(movie._id)}>
+                <span className="d-flex align-items-center">
+                  <i className="material-icons star mr-3">grade</i>
+                  Add favorite
+              </span>
+
+              </Button>
+              {/* <Link to="" onClick={() => this.addFavorite(movie._id)}>
+                <Button variant='link' className='m-3 goAdd'>Add Favorite</Button>
+              </Link>
+              <Link to="" onClick={() => this.deleteFavorite(movie._id)}>
+                <Button variant='link' className='m-3 goDelete'>Remove Favorite</Button>
+              </Link> */}
             </Row>
           </Card.Footer>
         </Card>
@@ -95,16 +161,28 @@ export class MovieView extends React.Component {
 
 MovieView.propTypes = {
   movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
     Title: PropTypes.string.isRequired,
+    Year: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      Name: PropTypes.string.isRequired
+    }).isRequired,
     BondActor: PropTypes.shape({
+      _id: PropTypes.string.isRequired,
       Name: PropTypes.string.isRequired,
     }).isRequired,
     Director: PropTypes.shape({
+      _id: PropTypes.string.isRequired,
       Name: PropTypes.string.isRequired,
     }).isRequired,
+    Actors: PropTypes.array.isRequired,
     Heroine: PropTypes.string.isRequired,
+    Villain: PropTypes.string.isRequired,
     ImagePath: PropTypes.string.isRequired,
-    ThumbNail: PropTypes.string.isRequired
+    ThumbNail: PropTypes.string.isRequired,
+    SongArtist: PropTypes.string.isRequired,
+    Featured: PropTypes.boolean,
   }).isRequired
 };
